7850c435636217e35fe89598924bb091809df771
jeremymacchiarella <jeremy.macchiarella@gmail.com>
2026-04-09 17:29:11 -0700

    created spyderbot class and some simple methods

--- src/main.py
+++ src/main.py
@@ -1,43 +1,12 @@
-from adafruit_servokit import ServoKit
-import time
-import sys
-
-
-SERVO_INIT_ANGLES = []
-kit = ServoKit(channels=16)
+from spyderbot import Spyderbot
+import atexit
 
 
 def main():
+    spyderbot = Spyderbot()
 
-    pass
-
-    
-
-def init_servo_pos():
-    for i in range(12):
-        kit.servo[i].angle = SERVO_INIT_ANGLES[i]
-
-def move_servo_slow(servo_idx, target_angle):
-    step = 1
-    delay = 0.02
-
-    current_angle = kit.servo[servo_idx].angle
-    if current_angle is None:
-        print(f"Error: Servo {servo_idx} angle is None. Cannot move servo.")
-        sys.exit()
-
-    while abs(current_angle - target_angle) > 0:
-        if current_angle < target_angle:
-            current_angle = min(current_angle + step, target_angle)
-        else:
-            current_angle = max(current_angle - step, target_angle)
-
-        kit.servo[servo_idx].angle = current_angle
-        time.sleep(delay)
-
-
-
-
+    # makes shutdown function run when this program exits
+    atexit.register(spyderbot.shutdown)
 
 
 
---
+++ src/spyderbot.py
@@ -0,0 +1,77 @@
+from adafruit_servokit import ServoKit
+import time
+import sys
+
+
+class Spyderbot:
+
+    def __init__(self):
+
+        # knees are even and hips are odd
+        # knees init to 60 and hips init to 90
+        self.SERVO_INIT_ANGLES = [60,90,60,90,60,90,60,90,60,90,60,90]
+        self.kit = ServoKit(channels=16) # keep at 16 even though we use 12
+        self.init_servo_pos()
+
+
+    def init_servo_pos(self):
+        for i in range(12):
+            self.kit.servo[i].angle = self.SERVO_INIT_ANGLES[i]
+
+    def move_servo_slow(self, servo_idx, target_angle): # not tested yet
+        step = 1
+        delay = 0.02
+
+        current_angle = self.kit.servo[servo_idx].angle
+        if current_angle is None:
+            print(f"Error: Servo {servo_idx} angle is None. Cannot move servo, exiting")
+            sys.exit()
+
+        while abs(current_angle - target_angle) > 0:
+            if current_angle < target_angle:
+                current_angle = min(current_angle + step, target_angle)
+            else:
+                current_angle = max(current_angle - step, target_angle)
+
+            self.kit.servo[servo_idx].angle = current_angle
+            time.sleep(delay)
+
+    def move_servo_slow_delta(self, servo_idx, delta):
+        
+
+        current_angle = self.kit.servo[servo_idx].angle
+        goal = int(current_angle + delta)
+        
+        if (goal > 180 or goal < 0):
+            print(f"error: trying to set invalid servo {servo_idx} angle, exiting")
+            sys.exit()
+
+        self.move_servo_slow(servo_idx, goal)
+
+    def move_servo(self, servo_idx, target_angle):
+        if (target_angle > 180 or target_angle < 0):
+            print(f"error: trying to set invalid servo {servo_idx} angle, exiting")
+            sys.exit()
+
+        self.kit.servo[servo_idx].angle = target_angle
+
+    def move_servo_delta(self, servo_idx, delta):
+        current_angle = self.kit.servo[servo_idx].angle
+        goal = int(current_angle + delta)
+        
+        if (goal > 180 or goal < 0):
+            print(f"error: trying to set invalid servo {servo_idx} angle, exiting")
+            sys.exit()
+
+        self.kit.servo[servo_idx].angle = goal
+
+    def shutdown(self):
+        for i in range(12):
+            # stop sending pwm signals to servos
+            self.kit.servo[i].angle = None
+
+        
+
+        
+
+
