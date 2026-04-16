34633d4293a30bd77d8f9c76c0821e888cdbdfb0
jeremymacchiarella <jeremy.macchiarella@gmail.com>
2026-04-16 12:41:01 -0700

    slow servo test

---
+++ test/test_slow_servo.py
@@ -0,0 +1,21 @@
+from spyderbot import Spyderbot
+import atexit
+import time
+
+
+def main():
+    spyderbot = Spyderbot()
+
+    spyderbot.move_servo_slow_delta(0, 90)
+    time.sleep(1)
+    spyderbot.move_servo_delta(0, -90)
+
+    
+
+    # makes shutdown function run when this program exits
+    atexit.register(spyderbot.shutdown)
+
+
+
+if __name__ == "__main__":
+    main()
