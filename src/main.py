b96a39b37b1c7a3738c42f9f833ea5783f5704a3
jeremymacchiarella <jeremy.macchiarella@gmail.com>
2026-04-16 13:38:09 -0700

    leg moving

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
 from spyderbot import Spyderbot
 import atexit
+import time
 
 
 def main():
     spyderbot = Spyderbot()
+    time.sleep(1)
 
     # makes shutdown function run when this program exits
     atexit.register(spyderbot.shutdown)
 
     spyderbot.lift_knee(0)
+    time.sleep(1)
     spyderbot.move_hip_forward(1)
+    time.sleep(1)
     spyderbot.lower_knee(0)
+    time.sleep(1)
     spyderbot.move_hip_backward(1)
 
 
