31200dfdccff6b71224617737b0772af4409d25a
jeremymacchiarella <jeremy.macchiarella@gmail.com>
2026-05-05 13:32:25 -0700

    teleop test

--- src/main.py
+++ src/main.py
@@ -15,63 +15,7 @@ def main():
 
         # spyderbot.turn_right()
         spyderbot.move_forward()
-        # spyderbot.lift_knee_slow(0)
-        # spyderbot.lift_knee_slow(4)
-        # spyderbot.lift_knee_slow(8)
-
         
-
-        # spyderbot.move_hips_forward_group_abs('left')
-
-        # spyderbot.lower_knees_group('left')
-
-        # spyderbot.lift_knees_group('right')
-
-        # spyderbot.move_hips_backward_group_abs('left')
-
-        # spyderbot.move_hips_forward_group_abs('right')
-
-        # spyderbot.lower_knees_group('right')
-
-        # spyderbot.lift_knees_group('left')
-
-        # spyderbot.move_hips_backward_group_abs('right')
-
-        
-
-
-
-     
-
-
-        
-
-        # time.sleep(1)
-
-       
-        # time.sleep(0.2)
-        # spyderbot.move_hip_forward_slow(1)
-        # spyderbot.move_hip_forward_slow(5)
-        # spyderbot.move_hip_forward_slow(9)
-        # time.sleep(0.2)
-        # spyderbot.lower_knee_slow(0)
-        # spyderbot.lower_knee_slow(4)
-        # spyderbot.lower_knee_slow(8)
-        # time.sleep(0.2)
-        # spyderbot.lift_knee_slow(2)
-        # spyderbot.lift_knee_slow(6)
-        # spyderbot.lift_knee_slow(10)
-        # time.sleep(0.2)
-        # spyderbot.move_hip_backward_slow(1)
-        # spyderbot.move_hip_backward_slow(5)
-        # spyderbot.move_hip_backward_slow(9)
-        # time.sleep(0.2)
-        # spyderbot.lower_knee_slow(2)
-        # spyderbot.lower_knee_slow(6)
-        # spyderbot.lower_knee_slow(10)
-        # time.sleep(0.2)
-
-    # spyderbot.lower_knees_group("left")
     time.sleep(1)
 
 
---
+++ src/teleop.py
@@ -0,0 +1,34 @@
+from spyderbot import Spyderbot
+import atexit
+import time
+import keyboard
+
+
+def main():
+    spyderbot = Spyderbot()
+    time.sleep(1)
+
+    # makes shutdown function run when this program exits
+    atexit.register(spyderbot.shutdown)
+
+    while (1):
+        if keyboard.is_pressed('w'):
+            print("forward")
+    
+        elif keyboard.is_pressed('a'):
+            print("turn left")
+    
+        elif keyboard.is_pressed('d'):
+            print("turn right")
+    
+        elif keyboard.is_pressed('s'):
+            print("backward")
+
+    
+        
+    time.sleep(1)
+
+
+
+if __name__ == "__main__":
+    main()
